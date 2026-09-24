fd7ea34d989a688b53fb512a6535f446e6a259ef
tswe-zin <tswe-zin@student.42singapore.sg>
2026-09-24 16:22:29 +0800

    dsdf

--- srcs/Server.cpp
+++ srcs/Server.cpp
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #include "Server.hpp"
 #include "Client.hpp"
 #include "Channel.hpp"
@@ -18,26 +17,3 @@ void Server::addChannel(std::string name, Channel* channel)
 {
     _channels.insert(std::make_pair(name, channel));
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Server.cpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ksan <ksan@student.42.sg>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/20 15:37:54 by ksan              #+#    #+#             */
-/*   Updated: 2026/09/24 15:55:43 by ksan             ###   ########.sg       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "Server.hpp"
-
-struct pollfd Server::create_fd( const int fd )
-{
-    struct pollfd _pollfd;
-    _pollfd.fd = fd;
-    _pollfd.events = POLLIN;
-    all_fds.push_back(_pollfd);
-}
->>>>>>> Stashed changes
